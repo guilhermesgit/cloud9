77d8aaa1d861beac524128890bc6bc7e3c4d8a34
Guilherme <guilhermecb10@hotmail.com>
2024-04-07 16:20:36 -0300

    fix: Corrigido os inputs

--- vars.tf
+++ vars.tf
@@ -1,26 +1,27 @@
 
-  variable "accountid" {
-    type = string
-  }
 
-  variable "accountname" {
-    type = string
-  }
+variable "accountid" {
+  type = string
+}
 
-  variable "instance_type" {
-    type = string
-  }
+variable "accountname" {
+  type = string
+}
 
-  variable "name" {
-    type = string
-  }
-  variable "image_id" {
-    type = string
-  }
+variable "instance_type" {
+  type = string
+}
 
-  variable "Ambiente" {
-    type = string
-  }
-  variable "connection_type" {
-    type = string
-  }
+variable "name" {
+  type = string
+}
+variable "image_id" {
+  type = string
+}
+
+variable "Ambiente" {
+  type = string
+}
+variable "connection_type" {
+  type = string
+}
