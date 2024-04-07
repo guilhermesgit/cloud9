2df0d55ca6d0429eeff4b479f1dcedf5d7822c60
Guilherme <guilhermecb10@hotmail.com>
2024-04-07 16:07:17 -0300

    feat: alterado modulo

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 
-module "cloud9" {
-  source          = "./modules/cloud9"
-  instance_type   = var.instance_type
-  name            = var.name
-  image_id        = var.image_id
-  Ambiente        = var.accountname
-  connection_type = var.connection_type
-  owner_arn       = "arn:aws:iam::${var.accountid}:${var.accountname}"
 
-}
+  module "cloud9" {
+    source          = "./modules/cloud9"
+    instance_type   = var.instance_type
+    name            = var.name
+    image_id        = var.image_id
+    Ambiente        = var.accountname
+    connection_type = var.connection_type
+    owner_arn       = "arn:aws:iam::${var.accountid}:${var.accountname}"
+
+  }
--- vars.tf
+++ vars.tf
@@ -1,25 +1,26 @@
-variable "accountid" {
-  type = string
-}
 
-variable "accountname" {
-  type = string
-}
+  variable "accountid" {
+    type = string
+  }
 
-variable "instance_type" {
-  type = string
-}
+  variable "accountname" {
+    type = string
+  }
 
-variable "name" {
-  type = string
-}
-variable "image_id" {
-  type = string
-}
+  variable "instance_type" {
+    type = string
+  }
 
-variable "Ambiente" {
-  type = string
-}
-variable "connection_type" {
-  type = string
-}
+  variable "name" {
+    type = string
+  }
+  variable "image_id" {
+    type = string
+  }
+
+  variable "Ambiente" {
+    type = string
+  }
+  variable "connection_type" {
+    type = string
+  }
